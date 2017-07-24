db101daf6c6f1f8af36199457164e49885b74d30
liuxx479 <liuxx479@gmail.com>
2017-07-24 17:50:46 -0400

    prep tar files

--- tar_files.py
+++ tar_files.py
@@ -15,15 +15,15 @@ def create_targz(i):
     cosmo_jia = cosmo_jia_arr[i]
     cosmo_apetri =cosmo_apetri_arr[i]
     bash_maps='''cd /scratch/02977/jialiu/lenstools_storage/{1}/1024b512
-tar -cvzf /scratch/02977/jialiu/neutrino_sims/convergence_maps/convergence_{0}.tar.gz Maps??'''.format(cosmo_jia, cosmo_apetri)
+tar -cvzf /scratch/02977/jialiu/neutrino_sims/convergence_maps/convergence_{0}.tar.gz Maps*'''.format(cosmo_jia, cosmo_apetri)
     bash_tree='''cd /scratch/02977/jialiu/temp/{0}/rockstar/trees
 tar -cvzf /scratch/02977/jialiu/neutrino_sims/trees/trees_{0}.tar.gz *'''.format(cosmo_jia)
     #if not os.path.isfile('/scratch/02977/jialiu/neutrino_sims/trees/trees_{0}.tar.gz'.format(cosmo_jia)):
     bash_rockstar = '''cd /scratch/02977/jialiu/temp/{0}/rockstar
     tar -cvzf /scratch/02977/jialiu/neutrino_sims/rockstar/rockstar_{0}.tar.gz out_*.list'''.format(cosmo_jia) 
     os.system(bash_maps)
-    os.system(bash_tree)
-    os.system(bash_rockstar)
+    #os.system(bash_tree)
+    #os.system(bash_rockstar)
     
     
 pool=MPIPool()
