141246910ac2dad7906e4f2ee24028a8e6b008f3
liuxx479 <liuxx479@gmail.com>
2017-08-08 11:05:58 -0400

    camb for stampede2

--- paramfilegen.py
+++ paramfilegen.py
@@ -286,7 +286,139 @@ l_sample_boost          = 3'''%(main_dir, filename, omch2, omnuh2, m1/M_nu, m2/M
     f.write(paramtext)
     f.close()
 
+def camb_gen_quick(M_nu, omega_m, A_s9):
+    print 'generating CAMB parameter files'
+    '''M_nu: total mass of neutrinos in unit of eV
+    A_s9 = A_s * 1e9
+    modify omch2, omnuh2, scalar_amp(1)
+    '''
+    if M_nu==0:
+        omnuh2=0
+    else:
+        omnuh2 = Mnu2Omeganu(M_nu, omega_m)*h**2
+    omch2 = omega_m*h**2 - omnuh2 - ombh2
+    filename = 'camb_mnv%.5f_om%.5f_As%.4f'%(M_nu, omega_m, A_s9)
+    m1, m2, m3 = neutrino_mass_calc (M_nu)
+    
+    paramtext='''#Parameters for CAMB
 
+#output_root is prefixed to output file names
+output_root = %scamb_out/%s
+
+get_scalar_cls = F
+get_vector_cls = F
+get_tensor_cls = F
+get_transfer   = T
+do_lensing     = F
+do_nonlinear = 0
+l_max_scalar      = 5000
+k_eta_max_scalar  = 8000
+l_max_tensor      = 1500
+k_eta_max_tensor  = 3000
+use_physical   = T
+########## cosmological parameters (Planck 2015 TT,TE,EE+lowP+lensing+ext)
+ombh2          = 0.02230
+omch2          = %.5f ##### = 0.1188 Planck2015
+omnuh2         = %.5f ##### = Mnu2Omeganu(M_nu, omega_m)eV 
+omk            = 0
+hubble         = 70
+w              = -1
+cs2_lam        = 1
+temp_cmb           = 2.7255
+helium_fraction    = 0.24
+
+massless_neutrinos = 0.046
+nu_mass_eigenstates = 3
+massive_neutrinos  = 1 1 1
+share_delta_neff = T
+nu_mass_fractions = %.5f %.5f %.5f
+nu_mass_degeneracies = 
+
+initial_power_num         = 1
+pivot_scalar              = 0.05
+pivot_tensor              = 0.05
+scalar_amp(1)             = %.4fe-9 ##### = 2.142e-9 Planck2015
+scalar_spectral_index(1)  = 0.97
+scalar_nrun(1)            = 0
+scalar_nrunrun(1)         = 0
+tensor_spectral_index(1)  = 0
+tensor_nrun(1)            = 0
+tensor_parameterization   = 1
+initial_ratio(1)          = 1
+
+reionization         = F
+re_use_optical_depth = T
+re_optical_depth     = 0.09
+re_redshift          = 11
+re_delta_redshift    = 1.5
+re_ionization_frac   = -1
+re_helium_redshift = 3.5
+re_helium_delta_redshift = 0.5
+RECFAST_fudge = 1.14
+RECFAST_fudge_He = 0.86
+RECFAST_Heswitch = 6
+RECFAST_Hswitch  = T
+
+initial_condition   = 1
+initial_vector = -1 0 0 0 0
+vector_mode = 0
+COBE_normalize = F
+CMB_outputscale = 7.42835025e12 
+
+transfer_high_precision = T
+transfer_kmax           = 30 ## 1000 for columbia, 25 for S. Bird
+transfer_k_per_logint   = 0
+transfer_num_redshifts  = 7
+transfer_interp_matterpower = F
+
+
+transfer_power_var = 0
+scalar_output_file = scalCls.dat
+vector_output_file = vecCls.dat
+tensor_output_file = tensCls.dat
+total_output_file  = totCls.dat
+lensed_output_file = lensedCls.dat
+lensed_total_output_file  =lensedtotCls.dat
+lens_potential_output_file = lenspotentialCls.dat
+
+do_lensing_bispectrum = F
+do_primordial_bispectrum = F
+bispectrum_nfields = 1
+bispectrum_slice_base_L = 0
+bispectrum_ndelta=3
+bispectrum_delta(1)=0
+bispectrum_delta(2)=2
+bispectrum_delta(3)=4
+bispectrum_do_fisher= F
+bispectrum_fisher_noise=0
+bispectrum_fisher_noise_pol=0
+bispectrum_fisher_fwhm_arcmin=7
+bispectrum_full_output_file=
+bispectrum_full_output_sparse=F
+bispectrum_export_alpha_beta=F
+
+feedback_level = 1
+output_file_headers = T
+derived_parameters = T
+lensing_method = 1
+accurate_BB = F
+
+massive_nu_approx = 1
+accurate_polarization   = F
+accurate_reionization   = F
+do_tensor_neutrinos     = T
+do_late_rad_truncation   = F
+halofit_version= 
+number_of_threads       = 0
+high_accuracy_default=T
+
+accuracy_boost          = 1
+l_accuracy_boost        = 1
+l_sample_boost          = 1'''%(main_dir, filename, omch2, omnuh2, m1/M_nu, m2/M_nu, m3/M_nu, A_s9)
+    
+    f = open('params/%s.param'%(filename), 'w')
+    f.write(paramtext)
+    f.close()
 
 def ngenic_gen(M_nu, omega_m, A_s9):
     print 'generating NGENIC parameter files'
@@ -1001,7 +1133,8 @@ for iparams in params:#param_restart:#
     #onu0_astropy, onu0_num =   Mnu2Omeganu(M_nu, omega_m), M_nu/93.04/h**2
     #print iparams, onu0_astropy, onu0_num, onu0_astropy/onu0_num-1.0
     
-    camb_gen(M_nu, omega_m, A_s9)
+    #camb_gen(M_nu, omega_m, A_s9)
+    camb_gen_quick(M_nu, omega_m, A_s9)
     #ngenic_gen(M_nu, omega_m, A_s9)
     #gadget_gen(M_nu, omega_m, A_s9)
     #outputs(iparams)
