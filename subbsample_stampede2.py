5bc18bfcbcfb396dcf78f772e7bd211f5452bc68
liuxx479 <liuxx479@gmail.com>
2017-10-10 14:41:12 -0400

    subsample for 101 models

--- subbsample_stampede2.py
+++ subbsample_stampede2.py
@@ -5,24 +5,26 @@ import sys
 import os
 
 cosmo_arr = genfromtxt('cosmo_jia_arr.txt',dtype='string')
-nsnaps_arr = loadtxt('nsnaps.txt')
+nsnaps_arr = loadtxt('nsnaps_cosmo_jia.txt')
 
 def subsample(jjj):
     cosmo = cosmo_arr[jjj]
     cosmo_dir = '/scratch/02977/jialiu/temp/'+cosmo
     nsnaps = nsnaps_arr[jjj]
-    for isnap in arange(nsnaps)[::-1]:
+    for isnap in arange(nsnaps):
         print cosmo, isnap
         INPUT_FILENAME = cosmo_dir + '/snapshots/snapshot_%03d'%(isnap)
         OUTPUT_DIR = cosmo_dir + '/snapshots_subsample/'
-        os.system('python subsample_gadget_snapshot.py %s %s' % (INPUT_FILENAME, OUTPUT_DIR))
+        out_fn = OUTPUT_DIR+'snapshot_%03d_idmod_101_0.hdf5'%(isnap)
+        if not os.path.isfile(out_fn):
+            os.system('python subsample_gadget_snapshot.py %s %s' % (INPUT_FILENAME, OUTPUT_DIR))
 
 pool=MPIPool()
 if not pool.is_master():
     pool.wait()
     sys.exit(0)
 
-pool.map(subsample, range(101)[::-1])
+pool.map(subsample, range(101))
 pool.close()
 
 print 'done-done-done'
