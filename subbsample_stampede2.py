c242c8322f5d66a51add999c2e06523ab11d3e88
liuxx479 <liuxx479@gmail.com>
2017-10-11 11:31:12 -0400

    subsample by snap

--- subbsample_stampede2.py
+++ subbsample_stampede2.py
@@ -19,12 +19,38 @@ def subsample(jjj):
         if not os.path.isfile(out_fn):
             os.system('python /work/02977/jialiu/neutrino-batch/subsample_gadget_snapshot.py %s %s' % (INPUT_FILENAME, OUTPUT_DIR))
 
+#pool=MPIPool()
+#if not pool.is_master():
+    #pool.wait()
+    #sys.exit(0)
+
+#pool.map(subsample, range(101))
+#pool.close()
+
+############## change to by snaps, not by cosmology
+all_snaps = []
+jjj=0
+for cosmo in cosmo_arr:
+    cosmo_dir = '/scratch/02977/jialiu/temp/'+cosmo
+    nsnaps = nsnaps_arr[jjj]
+    jjj+=1
+    for isnap in arange(nsnaps):
+        INPUT_FILENAME = cosmo_dir + '/snapshots/snapshot_%03d'%(isnap)
+        OUTPUT_DIR = cosmo_dir + '/snapshots_subsample/'
+        all_snaps.append[[INPUT_FILENAME, OUTPUT_DIR, isnap]]
+
+def subsample_bysnap(input3):
+    INPUT_FILENAME, OUTPUT_DIR, isnap = input3
+    out_fn = OUTPUT_DIR+'snapshot_%03d_idmod_101_0.hdf5'%(isnap)
+    if not os.path.isfile(out_fn):
+        os.system('python /work/02977/jialiu/neutrino-batch/subsample_gadget_snapshot.py %s %s' % (INPUT_FILENAME, OUTPUT_DIR))
+
 pool=MPIPool()
 if not pool.is_master():
     pool.wait()
     sys.exit(0)
 
-pool.map(subsample, range(101))
+pool.map(subsample_bysnap, all_snaps[::-1])
 pool.close()
 
 print 'done-done-done'
