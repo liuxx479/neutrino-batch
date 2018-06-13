1bc1e9283db4052ca5f5a671beb06c6e23da7acc
liuxx479 <liuxx479@gmail.com>
2018-06-13 13:32:58 -0400

    tar files

--- tar_files.py
+++ tar_files.py
@@ -31,8 +31,8 @@ tar -cvzf /scratch/02977/jialiu/neutrino_sims/trees/trees_{0}.tar.gz *'''.format
 tar -cvzf /scratch/02977/jialiu/neutrino_sims/rockstar/rockstar_{0}.tar.gz out_*.list'''.format(cosmo_jia) 
     bash_subsample = '''cd /scratch/02977/jialiu/temp/{0}/snapshots_subsample
 tar -cvzf /scratch/02977/jialiu/neutrino_sims/subsample/subsample_{0}.tar.gz *hdf5'''.format(cosmo_jia) 
-    #os.system(bash_maps_gal)
-    #os.system(bash_maps_CMB)
+    os.system(bash_maps_gal)
+    os.system(bash_maps_CMB)
     #os.system(bash_planes)
     #os.system(bash_tree)
     #os.system(bash_rockstar)
