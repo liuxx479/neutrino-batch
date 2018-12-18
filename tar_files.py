dfe33c3eab7e01a5f946f2402666f12ffd3405fb
liuxx479 <liuxx479@gmail.com>
2018-12-18 14:46:36 -0500

    tar 6redshift prep for ranch

--- tar_files.py
+++ tar_files.py
@@ -31,13 +31,16 @@ tar -cvzf /scratch/02977/jialiu/neutrino_sims/trees/trees_{0}.tar.gz *'''.format
 tar -cvzf /scratch/02977/jialiu/neutrino_sims/rockstar/rockstar_{0}.tar.gz out_*.list'''.format(cosmo_jia) 
     bash_subsample = '''cd /scratch/02977/jialiu/temp/{0}/snapshots_subsample
 tar -cvzf /scratch/02977/jialiu/neutrino_sims/subsample/subsample_{0}.tar.gz *hdf5'''.format(cosmo_jia) 
+    bash_maps_both2='''cd /scratch/02977/jialiu/neutrino_sims/convergence_maps/convergence_6redshifts_{0}
+tar -cvf /scratch/02977/jialiu/neutrino_sims/convergence_maps/convergence_6redshifts_{0}.tar *'''.format(cosmo_jia, cosmo_jia)
     #os.system(bash_maps_CMB)
-    os.system(bash_maps_gal)
+    #os.system(bash_maps_gal)
     #os.system(bash_planes)
     #os.system(bash_tree)
     #os.system(bash_rockstar)
     #os.system(bash_subsample)
     #os.system(bash_maps_both)
+    os.system(bash_maps_both2)
     
 def unzip(i):    
     cosmo_jia = cosmo_jia_arr[i]
@@ -89,10 +92,10 @@ if not pool.is_master():
     pool.wait()
     sys.exit(0)
 
-#pool.map(create_targz, range(101))
+pool.map(create_targz, range(101))
 #pool.map(unzip, range(101))
 #pool.map(unzip_maps_edison, range(101))
-pool.map(unzip_CMBmaps_edison, range(101))
+#pool.map(unzip_CMBmaps_edison, range(101))
 pool.close()
 
 print 'done-done-done'
