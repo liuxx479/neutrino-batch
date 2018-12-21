cbe89788d9b2a3849face72d287d94ce89b2b164
liuxx479 <liuxx479@gmail.com>
2018-12-21 12:34:31 -0500

    tar 6redshift prep for ranch

--- tar_files.py
+++ tar_files.py
@@ -102,8 +102,8 @@ if not pool.is_master():
     pool.wait()
     sys.exit(0)
 
-#pool.map(create_targz, range(101))
-pool.map(create_targz, [0,])
+pool.map(create_targz, range(101))
+#pool.map(create_targz, [0,])
 #pool.map(unzip, range(101))
 #pool.map(unzip_maps_edison, range(101))
 #pool.map(unzip_CMBmaps_edison, range(101))
